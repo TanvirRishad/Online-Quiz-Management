1227cdfc9d2e4510167ef53d74c94e60e14319f7
Tanvir2908 <118743779+TanvirRishad@users.noreply.github.com>
2026-01-20 03:05:42 +0600

    Delete view directory

--- view/validate_signin.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../controller/SigninController.php';
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $controller = new SigninController();
-    $controller->handleSignin($_POST);
-} else {
-    echo "Invalid request method.";
-}
